962292a4b01c6f23d07318abcad48520edaa0415
google-colabtools <fcmoabi@gmail.com>
2025-08-22 23:01:36 -0300

    Add files via upload

--- starter.sh
+++ starter.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 nohup gunicorn keep_service:app --bind 0.0.0.0:7860 &
 if [ "$RUN_ON_START" = "true" ]; then
-  bash run_daily.sh >/proc/1/fd/1 2>/proc/1/fd/2
+  bash runner_daily.sh >/proc/1/fd/1 2>/proc/1/fd/2
 fi
